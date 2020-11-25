c19ff111a7f386b898373896e2ac775c45d44c65
LaurentReese <74904892+LaurentReese@users.noreply.github.com>
2020-11-25 23:31:36 +0100

    First version showing some chassis
    I've let the node_modules empty as github complains that it contains too many files (which is true - about 800).
    The node_modules tree can be recreated by doing a "vue create" command.

---
+++ frontend/src/App.vue
@@ -0,0 +1,33 @@
+<template>
+  <div id="app">
+    <img alt="MTT logo" src="./assets/MTT.jpg" img width="5%">
+    <MTTChassis msg1="Bienvenue sur le site de MÉDITERRANÉE TECHNIQUES ET TRAVAUX" msg2="Présentation des chassis de fenêtre"/>
+  </div>
+</template>
+
+<script>
+import MTTChassis from './components/MTTChassis.vue'
+
+export default {
+  name: 'App',
+  // TODO : stuff like const msg1 = "Bienvenue sur le site de MÉDITERRANÉE TECHNIQUES ET TRAVAUX"
+  // TODO in mounted() ?
+  components: {
+    MTTChassis
+  },
+  data : function() {
+    return {}
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: left;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
