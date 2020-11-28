441399130bf8bb724b6e55287858ca23d2d54399
lreese1 <laurent.reese@free.fr>
2020-11-28 18:37:34 +0100

    Send a mail when a customer used the website

--- backend/serverGOLANG.go
+++ backend/serverGOLANG.go
@@ -4,6 +4,7 @@ import (
 	"encoding/json"
 	"fmt"
 	"net/http"
+	"net/smtp"
 )
 
 type receivedFromMTTchassis struct {
@@ -32,6 +33,8 @@ func mttChassis(w http.ResponseWriter, request *http.Request) {
 	decoder.Decode(&mttData)
 
 	fmt.Println(mttData)
+	sendMail(mttData)
+	// TO DO : + later => fill in a database with the record "mttData"
 
 	reponseData.Message = response	
 
@@ -44,7 +47,47 @@ func mttChassis(w http.ResponseWriter, request *http.Request) {
     }
 }
 
+func sendMail(info receivedFromMTTchassis) {
+ // Sender data.
+ from := "rene.lasurete@gmail.com" // old useless acount that I don't care about
+ password := "d<5@M48c6UyDz]" // password is here in clear but I don't care as it's an old useless acount
+
+ // Receiver email address.
+ to := []string{
+   "rene.lasurete@gmail.com",
+ }
+
+ // mail from myself to myself, just to create an entry to store a customer action ;-)
+
+ // smtp server configuration.
+ smtpHost := "smtp.gmail.com"
+ smtpPort := "587"
+
+ // Message.
+ //message := []byte("Test d'un message sur le MTT serveur.")
+ // TO DO ? : add date and time inside the message, although it is normally already in the mail itself
+ message := []byte ("Message reçu de la part de " + info.Prenom + " " + info.Nom + "\r\n" +
+                    "mail:" + info.Mail + "\r\n" +
+                    "téléphone:" + info.Telephone + "\r\n" +
+                    "==> intéressé par les chassis")
+ 
+ // Authentication.
+ auth := smtp.PlainAuth("", from, password, smtpHost)
+ 
+ // Sending email.
+ err := smtp.SendMail(smtpHost+":"+smtpPort, auth, from, to, message)
+ if err != nil {
+   fmt.Println(err)
+   return
+ }
+ fmt.Println("Email généré")
+}
+
+
 func main() {
+//	var test receivedFromMTTchassis
+//	sendMail(test)
+//	return
 	http.HandleFunc("/", mttChassis)
 	http.ListenAndServe(":8090", nil)
 }
