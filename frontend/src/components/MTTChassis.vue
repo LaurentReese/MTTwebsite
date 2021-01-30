3d66d68c6cdcbcfbc126784ae50a88aa0df18e5e
lreese1 <laurent.reese@free.fr>
2021-01-31 00:24:41 +0100

    Progress on the V2

--- frontend/src/components/MTTChassis.vue
+++ frontend/src/components/MTTChassis.vue
@@ -3,26 +3,56 @@
     <h1>{{ msg1 }}</h1>
     <h1>{{ msg2 }}</h1>
 
-
     <hr />
+    <b>
+    Monoslide/Chassis à ouverture totale : Ventaux aluminium à rupture de pont thermique, coulissant séparement sur toute la longueur d'un rail unique, et empilables dans une zone de stockage
+    </b>
+    <pre></pre>      
+    Prix : sur devis
+    <pre></pre>          
+    Délai de livraison : 6 à 8 semaines
+    <pre></pre>       
     <img
       alt="FENETRES PLIANTES PISCINE"
       src="../assets/FENETRES PLIANTES PISCINE.jpg"
       img
       width="40%"
     />
-    &nbsp;&nbsp;
-    <!--label-->
+    <pre></pre>       
+    <video
+      muted  = "true"
+      autoplay = "true"    
+      loop = "true"
+      src = "../assets/Fenetres Empilables Mediterranee Techniques Travaux.webm"    
+      ref="videoFenetresEmpilables"
+      width="40%"
+      horizontal-align=left
+      controls
+    ></video>
+    <pre></pre>    
+    <textarea
+      class="productText"
+      v-model="textProd2"
+      style="width:40%"
+      :rows="4"
+      :readonly="true"
+      :autoHeight="autoHeight"
+    ></textarea>
+    <pre></pre>
+    <a href="https://www.archiexpo.fr/prod/sunparadise/product-94948-1252311.html">{{VOIR_AUSSI}}</a>    
+    <pre></pre>    
     <input type="checkbox" v-model="produits[0]" />
     {{interet}}
-    <!--/label-->
-    <!--pre></pre-->
 
 
     <hr />
     <b>
     Supertherme 80 : Chassis à ventaux pliants empilables en accordéon
     </b>
+    <pre></pre>              
+    Prix : 700 Euros/m2
+    <pre></pre>              
+    Délai de livraison : 6 à 8 semaines
     <pre></pre>
     <img
       alt="FENETRES PLIANTES 5VTX"
@@ -55,19 +85,41 @@
     ></textarea>
     <!--button :disabled="isPlaying" @click="play">Jouer</button>
     <button :disabled="!isPlaying" @click="stop">Arrêter</button-->
+    <pre></pre>
+    <!--a href="www.sunparadise.com/FR/International/Consumer-Product-Detail/SYSTMES-DE-REPLIER/SUPERTHERM-80/0/2/6"></a-->
+    <a href="https://www.sunparadise.com/FR/International/Consumer-Product-Detail/SYSTMES-DE-REPLIER/SUPERTHERM-80/0/2/6">{{VOIR_AUSSI}}</a>    
     <pre></pre>    
     <input type="checkbox" v-model="produits[1]" />
     {{interet}}
 
 
     <hr />
+    <b>
+    Bali 60 : Panneau coulissant suspendu
+    </b>
+    <pre></pre>      
+    Prix : 700 Euros/m2
+    <pre></pre>          
+    Délai de livraison : 6 à 8 semaines
+    <pre></pre>       
     <img
       alt="FENETRES CORSE"
       src="../assets/FENETRES CORSE.jpg"
       img
       width="40%"
     />
-    &nbsp;&nbsp;
+    <pre></pre>    
+    <textarea
+      class="productText"
+      v-model="textProd3"
+      style="width:40%"
+      :rows="4"
+      :readonly="true"
+      :autoHeight="autoHeight"
+    ></textarea>    
+    <pre></pre>
+    <a href="https://www.archiexpo.fr/prod/sunparadise/product-62746-569452.html">{{VOIR_AUSSI}}</a>
+    <pre></pre>   
     <input type="checkbox" v-model="produits[2]" />
     {{interet}}
 
@@ -154,6 +206,7 @@ export default {
   data: function () {
     return {
       PLACE_HOLDER: "Ajoutez une ou plusieurs lignes",
+      VOIR_AUSSI : "Voir Aussi...",      
       // STRANGE : I want to make it work with stuff like v-model="produits[0]"
       // but declaring produits: Boolean[3] // does not work, I *must* use the following line instead (don't know why)
       produits: [false, false, false],
@@ -165,7 +218,9 @@ export default {
       messClient: "",
       addrTravaux: "",
       errors: [],
-      textProd1 : "Les cloisons pliantes suspendues Supertherme 80 offrent une multitude de possibilités d'exécution. Pliables vers la gauche, la droite, centrale ou bilatérales, vers l'intérieur ou l'extérieur. Les cloisons pliantes peuvent être réunies dans un angle avec un poteau fixe ou mobile. Largeur de 60 à 1200 mm, Hauteur de 1000 à 2700 mm. Vitrage de 24 à 62mm d'épaisseur. Les cadres ont un coefficient Uf de 1.8, combinés avec les vitrages adéquats ils permettent d'obtenir un coefficient Uw entre 0.8 et 1.4 Watt/m/K. Très facile à utiliser en rénovation."
+      textProd1 : "Les cloisons pliantes suspendues Supertherme 80 offrent une multitude de possibilités d'exécution. Pliables vers la gauche, la droite, centrale ou bilatérales, vers l'intérieur ou l'extérieur. Les cloisons pliantes peuvent être réunies dans un angle avec un poteau fixe ou mobile. Largeur de 60 à 1200 mm, Hauteur de 1000 à 2700 mm. Vitrage de 24 à 62mm d'épaisseur. Les cadres ont un coefficient Uf de 1.8, combinés avec les vitrages adéquats ils permettent d'obtenir un coefficient Uw entre 0.8 et 1.4 Watt/m/K. Très facile à utiliser en rénovation.",
+      textProd2 : "Combine fonctionnalité et flexibilité d'une manière unique: ouverture sans limites, même autour des coins, simplement en faisant coulisser la paroi sur le rail fixé au sol. Ce système avec rupture thermique permet de faire coulisser un nombre illimité de parois et de les empiler dans un coin. Ceci peut être combiné avec une porte tournante pour les entrées de tous les jours. La possibilité de faire coulisser les parois autours des coins allant jusqu'à 90° permet une grande liberté de design. Principales caractéristiques du système: flexibilité de par l'application d'option de rotation, Système en aluminium avec rupture de pont thermique, Angles de l'élément entre 90° et 180°, Largeur de l'élément: jusqu'à 1300mm, Hauteur de l'élément: jusqu'à 2700mm, Epaisseur du verre: jusqu'à 40mm.",
+      textProd3 : "Porte coulissante suspendue sur un à trois rails. Les éléments se soulèvent de 6mm par rotation manuelle de 180° de la poignée à levier. Les joints en caoutchouc sont ainsi libérés et l'élément peut coulisser facilement. Vitrage de 16 à 42mm. Dimensions des ventaux : largeur max = 2500mm, hauteur max = 2800 mm."
     };
   },
 
@@ -262,4 +317,32 @@ li {
 a {
   color: #42b983;
 }
+
+
+hr {
+	border-top: 5px double #8c8b8b;
+  border-style: solid; 
+}
+
+/*
+hr { 
+  height: 30px; 
+  border-style: solid; 
+  border-color: #8c8b8b; 
+  border-width: 1px 0 0 0; 
+  border-radius: 20px; 
+} 
+hr:before { 
+  display: block; 
+  content: ""; 
+  height: 30px; 
+  margin-top: -31px; 
+  border-style: solid; 
+  border-color: #8c8b8b; 
+  border-width: 0 0 1px 0; 
+  border-radius: 20px; 
+}
+*/
+
+
 </style>
