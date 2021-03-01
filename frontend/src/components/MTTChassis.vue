b82526b7f6dac4d7932e1e6c10e4c4edd45422cb
LaurentReese <74904892+LaurentReese@users.noreply.github.com>
2021-03-01 09:22:56 +0100

    Added useful link to use env. variable in vuejs

--- frontend/src/components/MTTChassis.vue
+++ frontend/src/components/MTTChassis.vue
@@ -288,6 +288,7 @@ export default {
 
       axios({
         method: "POST",
+// https://medium.com/js-dojo/how-to-use-environment-variables-in-vue-js-273eba0102b0 !!
         url: "http://127.0.0.1:8090/mttChassis",   
         //url: "http://golang.backend.mtt-habitat.laurentreese.node.intern:8090/mttChassis",           
         //url: "http://golang.backend.mtt-habitat:8090/mttChassis",                   
