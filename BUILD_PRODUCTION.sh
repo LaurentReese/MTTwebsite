a94a04bf553b2bb7b63800beccd1abad2407d78f
lreese1 <laurent.reese@free.fr>
2021-03-06 20:14:35 +0100

    script to build everything for PRODUCTION

---
+++ BUILD_PRODUCTION.sh
@@ -0,0 +1,28 @@
+# docker images | grep golang | awk '{$1="";$2="";$4="";$5="";$6="";$7="";$8="";$9="";$10="";print}'
+# to be executed when we are in the MTTwebsite directory
+
+# TO DO : verify that we are in the MTTwebsite directory
+set -x # verbose mode in order to follow what is happening
+sloppy delete mtt-habitat
+sloppy show
+
+docker rmi 32681733/mtt-frontend --force
+docker rmi 32681733/mtt-backend --force
+
+docker images purge
+docker images
+
+cd frontend
+cat .env.production.local
+docker build . -t 32681733/mtt-frontend
+docker push 32681733/mtt-frontend
+
+cd ../backend
+docker build . -t 32681733/mtt-backend
+docker push 32681733/mtt-backend
+
+cd ..
+sloppy start --var=domain:mtt-habitat.sloppy.zone mtt-sloppy.json
+sloppy show mtt-habitat
+
+echo "sloppy show mtt-habitat"
