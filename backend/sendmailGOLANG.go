54683be6ecabeab04e78034c70685eff49f4c9ce
lreese1 <laurent.reese@free.fr>
2021-03-16 20:29:58 +0100

    last checkins for beta version

--- backend/sendmailGOLANG.go
+++ backend/sendmailGOLANG.go
@@ -3,6 +3,8 @@ package main
 import (
 	//	"encoding/json"
 	"fmt"
+	"time"
+
 	//	"net/http"
 	"net/smtp"
 	"strconv"
@@ -15,16 +17,22 @@ func sendMail(info *receivedFromMTTchassis) {
 		return
 	}
 	// Sender data.
-	from := "rene.lasurete@gmail.com" // old useless account that I don't care about
-	password := "d<5@M48c6UyDz]"      // password is here in clear but I don't care as it's an old useless account
+	//from := "rene.lasurete@gmail.com" // old useless account that I don't care about
+	//password := "d<5@M48c6UyDz]"      // password is here in clear but I don't care as it's an old useless account
+	//from := "sasmtt06@gmail.com"
+	from := "site@sasmtt.fr"
+	password := "MTT_DYNAMIC_MAIL_PASSWORD"
 	// Receiver email address.
-	to := []string{"rene.lasurete@gmail.com"}
+	//	to := []string{"rene.lasurete@gmail.com"}
+	to := []string{"site@sasmtt.fr"}
 	// mail from myself to myself, just to create an entry to store a customer action ;-)
 	// smtp server configuration.
-	smtpHost := "smtp.gmail.com"
+	//smtpHost := "smtp.gmail.com"
+	smtpHost := "mail.gandi.net"
 	smtpPort := "587"
 	// TO DO ? : add date and time inside the message, although it is normally already in the mail itself
-	messageString := "Message reçu de la part de " + info.Prenom + " " + info.Nom + "\r\n" +
+	messageString := "Le " + time.Now().Format("2 January 2006 15:04:05") + "\r\n" +
+		"Message reçu de la part de " + info.Prenom + " " + info.Nom + "\r\n" +
 		"mail:" + info.Mail + "\r\n" +
 		"téléphone:" + info.Telephone + "\r\n" +
 		"==> intéressé(e) par le(s) produit(s) :" + "\r\n"
@@ -69,20 +77,24 @@ func sendMail(info *receivedFromMTTchassis) {
 func sendDatabaseByMail(dbName string) {
 	// See https://github.com/jordan-wright/email/blob/master/README.md
 	e := email.NewEmail()
-	e.From = "rene.lasurete@gmail.com"
-	password := "d<5@M48c6UyDz]" // password is here in clear but I don't care as it's an old useless account
-	e.To = []string{"rene.lasurete@gmail.com"}
+	//e.From = "rene.lasurete@gmail.com"
+	//password := "d<5@M48c6UyDz]" // password is here in clear but I don't care as it's an old useless account
+	// e.From = "sasmtt06@gmail.com"
+	e.From = "site@sasmtt.fr"
+	password := "MTT_DYNAMIC_MAIL_PASSWORD"
+	//e.To = []string{"rene.lasurete@gmail.com"}
+	e.To = []string{"site@sasmtt.fr"}
 	//e.Bcc = []string{"test_bcc@example.com"}
 	//e.Cc = []string{"test_cc@example.com"}
 	e.Subject = "MTT Database"
 	e.Text = []byte("Veuillez trouver la database MTT en pièce attachée.")
 	//e.HTML = []byte("<h1>Fancy HTML is supported, too!</h1>")
 	e.AttachFile(dbName)
-	err := e.Send("smtp.gmail.com:587",
+	err := e.Send("mail.gandi.net:587",
 		smtp.PlainAuth("",
 			e.From,
 			password,
-			"smtp.gmail.com"))
+			"mail.gandi.net"))
 	if err != nil {
 		fmt.Println(err)
 		return
